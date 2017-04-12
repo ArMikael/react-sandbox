0095eb74a483bb8cc69614a04384381a49dd16fe
Michael Treser <armikael2@gmail.com>
2017-04-12 18:07:29 +0300

    Updated Subscribe form to support all methods with ES6.

--- src/forms/Subscribe.js
+++ src/forms/Subscribe.js
@@ -1,34 +1,39 @@
 import React from 'react';
 
 class Subscribe extends React.Component {
-    getInitialState() {
-        return {
-            valid: false
-        }
+    constructor() {
+        super();
+
+        // When using ES6 syntax set default state in constructor instead of getInitialState
+        this.state = { valid: false };
+
+        // When using ES6 syntax bind this to all methods in constructor
+        this._validate = this._validate.bind(this);
+        this._submit = this._submit.bind(this);
     }
 
     _submit(event){
         event.preventDefault();
         console.log(event);
 
-        // if (this.state.valid) {
-        //     console.log('Submit is successful!');
-        // } else {
-        //     console.log('Invalid Email.');
-        // }
+        if (this.state.valid) {
+            console.log('Submit is successful!');
+        } else {
+            console.log('Invalid Email.');
+        }
     }
 
     _validate(event){
         const value = event.target.value;
-
-        this.setState({ valid: value.length > 2 });
+        this.setState({ valid: value.length > 4 });
+        // console.log('Validating...');
     }
 
     render(){
         return (
                 <form id="subscribeForm" onSubmit={this._submit}>
-                    <input type="email" id="email" onChange={this._validate} />
-                           {/*style={{ background: this.state.valid ? 'white' : 'red' }} />*/}
+                    <input type="email" id="email" onChange={this._validate}
+                           style={{ background: this.state.valid ? 'white' : 'pink' }} />
                     <button type="submit">Submit</button>
                 </form>
         );
