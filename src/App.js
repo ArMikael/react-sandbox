e8c59049463ab4d449009091851f2c452c31a7d6
Michael Treser <armikael2@gmail.com>
2017-04-28 11:54:22 +0300

    Added react-router-dom changes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-// import { Home } from './pages/Home';
+import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Timer } from './timer/Timer';
 
-const Home = () => <h1>Home</h1>;
+// const Home = () => <h1>Home</h1>;
 const Links = () => (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <NavLink exact activeClassName="active" to="/">Home</NavLink>
+        <NavLink exact activeClassName="active" to="/about">About</NavLink>
+        <NavLink exact activeClassName="active" to={{pathname: '/timer'}}>Timer</NavLink>
     </nav>
 );
 
@@ -22,6 +24,8 @@ class App extends React.Component {
                     <Links />
                     <Route exact path="/" component={ Home } />
                     <Route path="/about" component={ About } />
+                    <Route path="/timer" component={ Timer } />
+
                 </div>
             </Router>
         );
